3cfd16ea6c0cb181c9b11a6804eade9c11095771
smocken <stefan.mocken@congstar.net>
2026-03-09 11:01:18 +0100

    add url connection for compatibility

--- src/main/java/io/fusionauth/http/AbstractHttpHelper.java
+++ src/main/java/io/fusionauth/http/AbstractHttpHelper.java
@@ -18,8 +18,12 @@ package io.fusionauth.http;
 
 import io.fusionauth.jwks.JSONWebKeySetHelper;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -39,6 +43,49 @@ public abstract class AbstractHttpHelper {
 		HTTP_CLIENT = client;
 	}
 
+	protected static <T> T get(HttpURLConnection urlConnection, Function<InputStream, T> consumer, BiFunction<String, Throwable, ? extends RuntimeException> exception) {
+	    String endpoint = urlConnection.getURL().toString();
+
+	    try {
+	      urlConnection.setRequestMethod("GET");
+	      urlConnection.connect();
+	    } catch (Exception e) {
+	      throw exception.apply("Failed to connect to [" + endpoint + "].", e);
+	    }
+
+	    int status;
+	    try {
+	      status = urlConnection.getResponseCode();
+	    } catch (Exception e) {
+	      throw exception.apply("Failed to make a request to [" + endpoint + "].", e);
+	    }
+
+	    if (status < 200 || status > 299) {
+	      throw exception.apply("Failed to make a request to [" + endpoint + "], a status code of [" + status + "] was returned.", null);
+	    }
+
+	    try (InputStream is = new BufferedInputStream(urlConnection.getInputStream())) {
+	      return consumer.apply(is);
+	    } catch (Exception e) {
+	      throw exception.apply("Failed to parse the response as JSON from [" + endpoint + "].", e);
+	    }
+	  }
+
+	  protected static HttpURLConnection buildURLConnection(String endpoint) {
+	    try {
+	    	HttpURLConnection urlConnection = (HttpURLConnection) new URI(endpoint).toURL().openConnection();
+	    	urlConnection.setDoOutput(true);
+	    	urlConnection.setConnectTimeout(10_000);
+	    	urlConnection.setReadTimeout(10_000);
+	    	urlConnection.addRequestProperty("User-Agent", "fusionauth-jwt (https://github.com/FusionAuth/fusionauth-jwt)");
+	    	return urlConnection;
+	    } catch (IOException e) {
+	    	throw new JSONWebKeySetHelper.JSONWebKeySetException("Failed to build connection to [" + endpoint + "].", e);
+	    } catch (URISyntaxException e) {
+	    	throw new JSONWebKeySetHelper.JSONWebKeySetException("Failed to build connection to [" + endpoint + "].", e);
+		}
+	}
+	
 	protected static <T> T get(HttpRequest request, Function<InputStream, T> consumer,
 			BiFunction<String, Throwable, ? extends RuntimeException> exception) {
 		String endpoint = request.uri().toString();
